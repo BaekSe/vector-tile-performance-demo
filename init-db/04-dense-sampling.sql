c267c9946fdef491dec4ba4e480e6a7a32f1a5d9
SeungHoon Choi <b4ek.se@gmail.com>
2025-08-14 11:27:37 +0900

    Optimize raster tile size to 128x128 with 2px sampling interval
    - Change tile size from 256x256 to 128x128
    - Reduce sampling interval from 4px to 2px
    - Simplify to 2-pass sampling (3x3 + 2x2 blocks)
    - Adjust buffer size from 32 to 16 pixels

--- init-db/04-dense-sampling.sql
+++ init-db/04-dense-sampling.sql
@@ -1,7 +1,7 @@
--- 128x128 타일 + 2픽셀 간격 초조밀 샘플링
-DROP FUNCTION IF EXISTS raster_osm_polygons(integer, integer, integer);
+-- 4픽셀 간격의 더 조밀한 샘플링 버전 (품질 우선)
+DROP FUNCTION IF EXISTS raster_osm_polygons_dense(integer, integer, integer);
 
-CREATE OR REPLACE FUNCTION raster_osm_polygons(z integer, x integer, y integer)
+CREATE OR REPLACE FUNCTION raster_osm_polygons_dense(z integer, x integer, y integer)
 RETURNS bytea AS $$
 DECLARE
     rast raster;
@@ -10,7 +10,7 @@ DECLARE
     pixel_size_x float;
     pixel_size_y float;
     buildings geometry;
-    buffer_pixels integer := 16;  -- 128에 맞게 버퍼 감소
+    buffer_pixels integer := 64;
     building_count integer;
     pixel_x integer;
     pixel_y integer;
@@ -19,7 +19,7 @@ DECLARE
     test_point geometry;
     px integer;
     py integer;
-    tile_size integer := 128;
+    tile_size integer := 512;
 BEGIN
     -- 줌 레벨 제한
     IF z < 5 OR z > 21 THEN
@@ -44,14 +44,13 @@ BEGIN
     WHERE way && buffered_envelope
     AND building IS NOT NULL;
     
-    -- 128x128 래스터 생성
+    -- 512x512 래스터 생성
     rast := ST_MakeEmptyRaster(tile_size, tile_size, 
                                ST_XMin(envelope_3857), ST_YMax(envelope_3857),
                                pixel_size_x, -pixel_size_y, 
                                0, 0, 3857);
     rast := ST_AddBand(rast, 1, '8BUI'::text, 0, 0);
     
-    -- 건물이 있으면 초조밀하게 샘플링
     IF building_count > 0 THEN
         -- 건물 지오메트리 수집
         SELECT ST_Union(way) INTO buildings
@@ -63,16 +62,16 @@ BEGIN
         buildings := ST_Intersection(buildings, envelope_3857);
         
         IF buildings IS NOT NULL AND NOT ST_IsEmpty(buildings) THEN
-            -- 2픽셀 간격으로 3x3 블록
-            FOR pixel_x IN 0..(tile_size-1) BY 2 LOOP
-                FOR pixel_y IN 0..(tile_size-1) BY 2 LOOP
-                    world_x := ST_XMin(envelope_3857) + (pixel_x + 1) * pixel_size_x;
-                    world_y := ST_YMax(envelope_3857) - (pixel_y + 1) * pixel_size_y;
+            -- 4픽셀 간격으로 6x6 블록 (더 조밀한 샘플링)
+            FOR pixel_x IN 0..(tile_size-1) BY 4 LOOP
+                FOR pixel_y IN 0..(tile_size-1) BY 4 LOOP
+                    world_x := ST_XMin(envelope_3857) + (pixel_x + 2) * pixel_size_x;
+                    world_y := ST_YMax(envelope_3857) - (pixel_y + 2) * pixel_size_y;
                     test_point := ST_SetSRID(ST_Point(world_x, world_y), 3857);
                     
                     IF ST_Intersects(buildings, test_point) THEN
-                        FOR px IN pixel_x..LEAST(pixel_x+2, tile_size-1) LOOP
-                            FOR py IN pixel_y..LEAST(pixel_y+2, tile_size-1) LOOP
+                        FOR px IN pixel_x..LEAST(pixel_x+5, tile_size-1) LOOP
+                            FOR py IN pixel_y..LEAST(pixel_y+5, tile_size-1) LOOP
                                 rast := ST_SetValue(rast, 1, px + 1, py + 1, 255);
                             END LOOP;
                         END LOOP;
@@ -80,9 +79,26 @@ BEGIN
                 END LOOP;
             END LOOP;
             
-            -- 1픽셀 오프셋으로 2x2 블록 (빈 공간 메우기)
-            FOR pixel_x IN 1..(tile_size-1) BY 2 LOOP
-                FOR pixel_y IN 1..(tile_size-1) BY 2 LOOP
+            -- 2픽셀 오프셋으로 4x4 블록 (빈 공간 메우기)
+            FOR pixel_x IN 2..(tile_size-1) BY 4 LOOP
+                FOR pixel_y IN 2..(tile_size-1) BY 4 LOOP
+                    world_x := ST_XMin(envelope_3857) + (pixel_x + 1) * pixel_size_x;
+                    world_y := ST_YMax(envelope_3857) - (pixel_y + 1) * pixel_size_y;
+                    test_point := ST_SetSRID(ST_Point(world_x, world_y), 3857);
+                    
+                    IF ST_Intersects(buildings, test_point) THEN
+                        FOR px IN pixel_x..LEAST(pixel_x+3, tile_size-1) LOOP
+                            FOR py IN pixel_y..LEAST(pixel_y+3, tile_size-1) LOOP
+                                rast := ST_SetValue(rast, 1, px + 1, py + 1, 255);
+                            END LOOP;
+                        END LOOP;
+                    END IF;
+                END LOOP;
+            END LOOP;
+            
+            -- 1픽셀 오프셋으로 2x2 블록 (더 완전한 메움)
+            FOR pixel_x IN 1..(tile_size-1) BY 4 LOOP
+                FOR pixel_y IN 1..(tile_size-1) BY 4 LOOP
                     world_x := ST_XMin(envelope_3857) + (pixel_x + 0.5) * pixel_size_x;
                     world_y := ST_YMax(envelope_3857) - (pixel_y + 0.5) * pixel_size_y;
                     test_point := ST_SetSRID(ST_Point(world_x, world_y), 3857);
@@ -102,11 +118,10 @@ BEGIN
     RETURN ST_AsPNG(rast);
     
 EXCEPTION WHEN OTHERS THEN
-    -- 예외 발생 시 투명한 128x128 PNG 반환
     rast := ST_MakeEmptyRaster(tile_size, tile_size, 0, 0, 1, -1, 0, 0, 3857);
     rast := ST_AddBand(rast, 1, '8BUI'::text, 0, 0);
     RETURN ST_AsPNG(rast);
 END;
 $$ LANGUAGE plpgsql STABLE;
 
-COMMENT ON FUNCTION raster_osm_polygons(integer, integer, integer) IS '{"format": "png"}';
+COMMENT ON FUNCTION raster_osm_polygons_dense(integer, integer, integer) IS '{"format": "png"}';
